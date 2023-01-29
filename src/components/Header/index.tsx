d7c2f96670b7862ed02128eddff50df927ec01ab
SheilaNS <shei.nsantos@gmail.com>
2023-01-29 13:00:34 -0300

    style: :art: Header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import { HeaderContainer, HeaderContent, CartButton } from "./styles";
+import logo from "../../assets/logo.svg";
+import cart from "../../assets/cart-icon.svg";
+
 export function Header() {
   return (
-    <h1>Header</h1>
+    <HeaderContainer>
+      <HeaderContent>
+        <Image src={logo} alt="" />
+
+        <CartButton>
+          <Image src={cart} alt="" />
+          <span>0</span>
+        </CartButton>
+      </HeaderContent>
+    </HeaderContainer>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,36 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 100px;
+  background-color: ${(props) => props.theme.primary};
+`
+
+export const HeaderContent = styled.div`
+  max-width: 90%;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 28px 0;
+`
+
+export const CartButton = styled.button`
+  width: 90px;
+  height: 45px;
+  border-radius: 8px;
+  border: 0;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 16px;
+  padding-left: 15px;
+  cursor: pointer;
+
+  background-color: ${(props) => props.theme.white};
+  color: ${(props) => props.theme.black};
+
+  span {
+    font-weight: 700;
+    font-size: 18px;
+  }
+`
