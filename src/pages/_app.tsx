0169237be00d1b9f759a739ee818e74b8d7a3fce
SheilaNS <shei.nsantos@gmail.com>
2023-01-29 12:55:32 -0300

    feat: :sparkles: Add Header and Footer in products page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+import { GlobalStyle } from '@/styles/global';
+import { defaultTheme } from '@/styles/themes/default';
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
+  );
 }
