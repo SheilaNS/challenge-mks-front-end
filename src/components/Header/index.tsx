ed7dade9eea8ab61b37f90a6f682e32ccb23e9dc
SheilaNS <shei.nsantos@gmail.com>
2023-01-31 03:10:42 -0300

    feat: :sparkles: Add cart total products

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 import { HeaderContainer, HeaderContent, CartButton } from "./styles";
 import * as Dialog from "@radix-ui/react-dialog";
 import logo from '../../assets/logo.svg';
-import cart from '../../assets/cart-icon.svg';
+import cartIcon from '../../assets/cart-icon.svg';
 import { CartModal } from "../CartModal";
+import { useAppSelector } from "@/hooks";
 
 export function Header() {
+  const cart = useAppSelector((state) => state.cart);
+
   return (
     <HeaderContainer>
       <HeaderContent>
@@ -14,8 +17,8 @@ export function Header() {
         <Dialog.Root>
           <Dialog.Trigger asChild>
             <CartButton>
-            <Image src={cart} alt="" />
-            <span>0</span>
+            <Image src={cartIcon} alt="" />
+            <span>{cart.items.length}</span>
             </CartButton>
           </Dialog.Trigger>
 
