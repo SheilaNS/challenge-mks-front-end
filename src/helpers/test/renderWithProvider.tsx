408f4d71de7086ec9bf7af6ffcf6b3c86885d2a6
SheilaNS <shei.nsantos@gmail.com>
2023-01-31 17:12:41 -0300

    test: :white_check_mark: Jest config

---
+++ src/helpers/test/renderWithProvider.tsx
@@ -0,0 +1,18 @@
+import { store } from "@/redux/store";
+import { GlobalStyle } from "@/styles/global";
+import { defaultTheme } from "@/styles/themes/default";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+
+export function renderWithProvider( children: React.ReactElement) {
+  return {
+    ...render(
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+
+        <Provider store={store}>{children}</Provider>
+      </ThemeProvider>
+    ),
+  };
+}
